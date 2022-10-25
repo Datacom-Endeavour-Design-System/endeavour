f016b343b53251e6227e3845f1f09536fa774fe5
Jon Vivian <jonv@datacom.co.nz>
2022-10-26 11:55:28 +1300

    add cypress for component testing

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -4,6 +4,11 @@ import tsconfigpaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    fs: {
+      strict: false
+    }
+  },
   plugins: [
     tsconfigpaths(), 
     react()
