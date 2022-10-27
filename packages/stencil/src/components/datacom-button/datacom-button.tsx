416710eb86001d190cd704b9f3a0653c463a0fb7
Andrew Wheeler <andrewwh@datacom.co.nz>
2022-10-27 13:24:28 +1300

    Fixed hover and focus states for button and changed to scoped component

--- packages/stencil/src/components/datacom-button/datacom-button.tsx
+++ packages/stencil/src/components/datacom-button/datacom-button.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Element, Listen} from '@stencil/core';
+import { Component, Host, h, Prop, Element} from '@stencil/core';
 import {getSvg} from '../../common/images/icon-provider';
 
 export type ButtonVariant = 'primary' | 'seconday' | 'ghost';
@@ -17,12 +17,15 @@ export type ButtonType = 'button' | 'submit' | 'reset';
  * icon = svg icon name
  * loading = true | false to show spinning icon
  * 
+ * This button cannot exist within the shadow root as it sits outside
+ * the DOM flow with regards to form submit.
+ * 
  * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
  */
 @Component({
   tag: 'datacom-button',
   styleUrl: 'datacom-button.css',
-  shadow: true,
+  scoped: true,
 })
 export class DatacomButton {
   @Element() el: HTMLElement;
@@ -47,42 +50,6 @@ export class DatacomButton {
   @Prop() formenctype: string;
   @Prop() formaction: string;
   @Prop() formtarget: string;
-
-  /**
-   * For submit buttons within a form the shadow dom elements are "outside" the form tree. To rectify this
-   * the component must intercept the click event and temporarily create a button within the form.
-   * 
-   * @returns void
-   */
-  @Listen('click')
-  onClick() {
-    if (this.type !== 'submit') {
-      return;
-    }
-
-    let form: HTMLFormElement;
-    if (this.form) {
-      form = document.querySelector(`#${this.form}`);
-    } else {
-      form = this.el.closest('form');
-    }
-
-    if (form) {
-      const btn = document.createElement('button');
-      btn.type = 'submit';
-      btn.style.display = 'none';
-      if (this.name) btn.name = this.name;
-      if (this.value) btn.value = this.value;
-      if (this.formmethod) btn.formMethod = this.formmethod;
-      if (this.formaction) btn.formAction = this.formaction;
-      if (this.formtarget) btn.formTarget = this.formtarget;
-      if (this.formenctype) btn.formEnctype = this.formenctype;
-
-      form.appendChild(btn);
-      btn.click();
-      btn.remove();
-    }
-  }
 
   render() {
     if (!['primary','secondary', 'ghost'].includes(this.variant)) {
@@ -123,6 +90,13 @@ export class DatacomButton {
     return (
       <Host>
         <button type={this.type}
+            form={this.form}
+            name={this.name}
+            value={this.value}
+            formmethod={this.formmethod}
+            formaction={this.formaction}
+            formtarget={this.formtarget}
+            formenctype={this.formenctype}
             autoFocus={this.autofocus} 
             aria-labelledby="button-text" 
             disabled={this.disabled} 
