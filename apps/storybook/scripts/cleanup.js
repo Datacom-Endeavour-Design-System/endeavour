f71269328cea19bf5cd5bdba1b014fb987338e70
Jon Vivian <2586037+4leite@users.noreply.github.com>
2022-10-28 10:48:30 +1300

    Bump storybook theme version and fix cloudflare deployment

---
+++ apps/storybook/scripts/cleanup.js
@@ -0,0 +1,8 @@
+
+const fs = require('fs');
+const replace = require('replace-in-file');
+
+// cloudflare pages doesn't like node_modules and @ characters
+fs.renameSync('./storybook-static/sb-addons/node_modules/@storybook', './storybook-static/sb-addons/storybook');
+// fix references to the moved files
+replace.sync({ files: './storybook-static/*', from: /node_modules\/%40storybook/g, to: 'storybook' });
