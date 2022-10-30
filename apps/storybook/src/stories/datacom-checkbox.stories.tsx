328665a87d49b5e4fafd614664c328070155898e
Jon Vivian <jonv@datacom.co.nz>
2022-10-31 01:24:26 +1300

    fix typescript settings for better type inference

--- apps/storybook/src/stories/datacom-checkbox.stories.tsx
+++ apps/storybook/src/stories/datacom-checkbox.stories.tsx
@@ -1,35 +1,34 @@
-import React, {useState} from 'react';
-import { ComponentStoryFn } from '@storybook/react';
-import { DatacomCheckbox } from '@datacom/endeavour-react';
-import {useEventRef} from '@datacom/endeavour-react';
+import React, { useState } from 'react';
+import { ComponentStoryFn, Meta } from '@storybook/react';
+import { DatacomCheckbox, useEventRef } from '@datacom/endeavour-react';
 
 export default {
   title: 'Checkbox',
-  component: DatacomCheckbox
-};
+  component: DatacomCheckbox,
+} as Meta<typeof DatacomCheckbox>;
 
-const Template: ComponentStoryFn<typeof DatacomCheckbox> = (args) => <DatacomCheckbox {...args} />;
+const Template: ComponentStoryFn<typeof DatacomCheckbox> = (args) => (
+  <DatacomCheckbox {...args} />
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
-  label: 'Tick Me!'
+  label: 'Tick Me!',
 };
 
 const CheckboxWrapperToggle: React.FC = () => {
   const [toggled, setToggled] = useState(false);
 
-  const ref = useEventRef<HTMLDatacomCheckboxElement>('toggle', (e: CustomEvent<boolean>) => {
-    setToggled(e.detail);
+  const ref = useEventRef<HTMLDatacomCheckboxElement>('toggle', (e: Event) => {
+    setToggled((e as CustomEvent<boolean>).detail);
   });
-  
+
   return (
-      <div>
-        <DatacomCheckbox label="With toggle" ref={ref} />
-        {
-        toggled && <p>You clicked the checkbox!</p>
-        }
-      </div>
+    <div>
+      <DatacomCheckbox label="With toggle" ref={ref} />
+      {toggled && <p>You clicked the checkbox!</p>}
+    </div>
   );
 };
 
-export const Toggle = () => <CheckboxWrapperToggle/>;
+export const Toggle = () => <CheckboxWrapperToggle />;
