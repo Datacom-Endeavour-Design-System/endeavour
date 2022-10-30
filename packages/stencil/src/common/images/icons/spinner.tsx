e1fcfd1061c61f2b1e84c43a28108fd19145ea5f
Andrew Wheeler <andrewwh@datacom.co.nz>
2022-10-31 11:16:03 +1300

    Revert "Disabled lint rule no-unsafe-return for icon tsx components"
    This reverts commit b9e187328ca8e2759b01944c31ac20d7b0d135fb.

--- packages/stencil/src/common/images/icons/spinner.tsx
+++ packages/stencil/src/common/images/icons/spinner.tsx
@@ -1,7 +1,6 @@
 import { h } from '@stencil/core';
 import { SVGOpts } from '../icons-opts';
 
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 export const Spinner = (opts?: SVGOpts) => 
     <svg class={opts?.class} viewBox="0 0 50 50">
         <circle cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
