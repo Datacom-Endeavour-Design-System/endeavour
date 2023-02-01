70a0ebdc02b5835f8fddb920afb04f1fb968cbce
angecast <angelc@datacom.co.nz>
2023-02-01 15:25:51 +1300

    Cleaning up textarea stories

--- apps/storybook/src/stories/datacom-textarea.stories.tsx
+++ apps/storybook/src/stories/datacom-textarea.stories.tsx
@@ -1,93 +1,56 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { ComponentStoryFn, Meta } from '@storybook/react';
 import { DatacomTextarea, DatacomButton } from '@datacom/endeavour-react';
+
 export default {
   title: 'Text Area',
   component: DatacomTextarea,
   argTypes: {
-    label: {
-      name: 'Label',
-      defaultValue: 'Textarea label',
-      description: 'Textarea label',
-      type: { name: 'string', required: true },
-    },
     name: {
       name: 'Name',
-      defaultValue: 'Textarea Name',
-      description: 'Textarea Name',
+      defaultValue: 'Name',
+      description: 'Name attribute for text area element',
+      type: { name: 'string', required: true },
+    },
+    label: {
+      name: 'Label',
+      defaultValue: 'Label',
+      description: 'Label for text area element',
       type: { name: 'string', required: true },
     },
     value: {
       name: 'Value',
-      description: 'Prepopulated input',
+      description: 'Prepopulated input value',
       type: { name: 'string', required: false },
     },
-    placeholder: {
-      name: 'Placeholder',
-      defaultValue: 'Enter a value',
-      description: 'Input placeholder prompt',
-      type: { name: 'string', required: false },
-    },
-    rows: {
-      name: 'Rows',
-      description: 'rows of textarea ',
-      type: { name: 'number' },
-    },
-    cols: {
-      name: 'Cols',
-      description: 'columns of textarea ',
-      type: { name: 'number' },
-    },
-    counter: {
-      name: 'Character Counter',
-      description: 'Charaters counts in the textarea field ',
-      type: { name: 'number', required: false },
-    },
-
     message: {
       name: 'Error Message',
+      defaultValue: 'Please complete this field.',
       description:
         'Error if validation fails or explicitly set with "valid" property',
       type: { name: 'string', required: false },
     },
     help: {
-      name: 'Help text',
+      name: 'Help Text',
       description: 'Assistance instructions below input',
       type: { name: 'string', required: false },
     },
-    valid: {
-      name: 'Valid',
+    isValid: {
+      name: 'Is Valid',
       description: 'Is the input valid (show error otherwise)',
       type: { name: 'boolean' },
     },
-    required: {
-      name: 'Required',
-      description: 'Is the input required',
-      type: { name: 'boolean' },
-    },
-
-    minlength: {
-      name: 'Min length',
-      description: 'Maximum number of characters',
-      type: { name: 'number' },
-    },
     maxlength: {
-      name: 'Max length',
+      name: 'Max Character Length',
       description: 'Maximum number of characters',
       type: { name: 'number' },
     },
     disabled: {
       name: 'Disabled',
+      defaultValue: false,
       description: 'Disable button',
       type: { name: 'boolean' },
     },
-  },
-  args: {
-    label: 'Label',
-    disabled: false,
-    required: true,
-    message: 'Fill this.',
-    vaild: false,
   },
 } as Meta<typeof DatacomTextarea>;
 const Template: ComponentStoryFn<typeof DatacomTextarea> = (args) => (
@@ -96,24 +59,26 @@ const Template: ComponentStoryFn<typeof DatacomTextarea> = (args) => (
     <DatacomTextarea {...args} />
   </div>
 );
+
 export const Standard = Template.bind({});
-Standard.args = {};
+
 export const HelperText = Template.bind({});
 HelperText.args = {
-  help: 'Make sure complete this.',
+  help: 'Make sure to complete this field.',
 };
+
 export const WithError = Template.bind({});
 WithError.args = {
-  message: 'Please Complet this field.',
-  label: 'Label',
-  valid: false,
+  isValid: false,
 };
+
 export const Disabled = Template.bind({});
 Disabled.args = {
   label: 'Disabled',
   disabled: true,
 };
-export const FormSubmition = () => {
+
+export const FormSubmission = () => {
   const form = useRef<HTMLFormElement>();
   const [submitted, setSubmitted] = useState(false);
 
@@ -125,20 +90,20 @@ export const FormSubmition = () => {
       setSubmitted(false);
     }
   };
+
   return (
     <form method="post" ref={form} onSubmit={handleSubmit}>
       <div style={{ width: '300px' }}>
         <DatacomTextarea
           name="textarea"
           required
-          autoValidat
-          message="Please enter value">
-          {' '}
-          Label
-        </DatacomTextarea>
+          autoValidate
+          message="Please enter value"
+          label="Label"></DatacomTextarea>
       </div>
       <br />
-      <div style={{ margin: '24px' }}>
+      {submitted && <p>Form would have been submitted but was prevented</p>}
+      <div style={{ marginTop: '24px' }}>
         <DatacomButton variant="primary" type="submit">
           Submit
         </DatacomButton>
