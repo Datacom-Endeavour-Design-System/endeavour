2230bb768774327815e4a96208c228335842a5ed
sidra2022 <98070825+sidra2022@users.noreply.github.com>
2023-02-17 11:23:23 +1300

    Breadcrumb (#43)
    * Breadcrumb component
    
    * Add Breadcrumb component
    
    * Remove the breadcrumb group logic
    
    * Feedback updates
    
    * update the usage.md
    
    * Remove the Text prop

---
+++ apps/storybook/src/stories/datacom-breadcrumb.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ComponentStoryFn } from '@storybook/react';
+import { DatacomBreadcrumb } from '@datacom/endeavour-react';
+
+export default {
+  title: 'Breadcrumb',
+  component: DatacomBreadcrumb,
+  argTypes: {
+    url: {
+      name: 'URL',
+      defaultValue: 'https://datacom.com/',
+      type: { text: 'string' },
+      options: [' ', 'https://datacom.com/'],
+    },
+  },
+};
+
+const Template: ComponentStoryFn<typeof DatacomBreadcrumb> = (args) => {
+  const { url } = args;
+  return <DatacomBreadcrumb url={url}>Breadcrumb</DatacomBreadcrumb>;
+};
+export const Breadcrumb = Template.bind({});
+
+export const BreadcrumbGrouped = () => {
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <DatacomBreadcrumb url="https://datacom.com">Home</DatacomBreadcrumb>
+      <DatacomBreadcrumb url="/nz/co">Breadcrumb 1</DatacomBreadcrumb>
+      <DatacomBreadcrumb url="/nz/co">Breadcrumb 2</DatacomBreadcrumb>
+      <DatacomBreadcrumb url="/nz/co">Breadcrumb 3</DatacomBreadcrumb>
+    </div>
+  );
+};
