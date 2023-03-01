64676d8a2b4c61af194f9b7111f22d233c085c94
angecast <angelc@datacom.co.nz>
2023-03-02 10:56:58 +1300

    Fixing radio responsive wrapping + function commenting

--- packages/stencil/src/components/datacom-card/datacom-selection-card.tsx
+++ packages/stencil/src/components/datacom-card/datacom-selection-card.tsx
@@ -25,15 +25,28 @@ export class DatacomSelectionCard {
 
   private children: HTMLDatacomRadioElement[] = [];
 
-  private onSubmitClick = (e: Event) => {
-    e.preventDefault();
+  /**
+   * Click handler function when submit button is clicked.
+   *
+   * @param event - Click event of submit button
+   */
+  private onSubmitClick = (event: Event) => {
+    event.preventDefault();
     this.submitClicked.emit(this.optionSelected);
   };
 
+  /**
+   * Click handler function when expand button is clicked.
+   */
   private onExpandClick = () => {
     this.expanded = !this.expanded;
   };
 
+  /**
+   * Returns all radio components within this component.
+   *
+   * @returns
+   */
   private getRadioComponents(): HTMLDatacomRadioElement[] {
     if (this.host == undefined) {
       return [];
@@ -43,15 +56,25 @@ export class DatacomSelectionCard {
       return this.children;
     }
 
-    this.host.querySelectorAll<HTMLDatacomRadioElement>('datacom-radio').forEach(t => this.children.push(t));
+    this.host.querySelectorAll<HTMLDatacomRadioElement>('datacom-radio').forEach(element => this.children.push(element));
     return this.children;
   }
 
+  /**
+   * Listener function on radio components.
+   * Used to update currently selected option in state.
+   *
+   * @param event - Custom 'selected' state from radio component.
+   */
   @Listen('selected', { capture: true })
   onRadioChange(event: CustomEvent<string>) {
     this.optionSelected = event.detail;
   }
 
+  /**
+   * Lifecycle function which applies shared name value to
+   * radio components, establishing them as a radio group.
+   */
   async componentWillLoad(): Promise<void> {
     const children = this.getRadioComponents();
     children.forEach(option => {
@@ -86,8 +109,10 @@ export class DatacomSelectionCard {
               </button>
             </div>
             <form class="dc-card-form">
-              <div class="dc-card-options">
-                <slot name="options" />
+              <div class="dc-card-options-wrapper">
+                <div class="dc-card-options">
+                  <slot name="options" />
+                </div>
               </div>
               <datacom-button variant="secondary" size="small" type="submit" onClick={this.onSubmitClick}>
                 {this.ctaText}
