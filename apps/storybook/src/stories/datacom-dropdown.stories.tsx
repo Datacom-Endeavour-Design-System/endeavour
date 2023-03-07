daa7433502097cb68adcfe04bb0f4ae51ea2f901
angecast <angelc@datacom.co.nz>
2023-03-07 17:40:13 +1300

    Updating stories with isValid prop

--- apps/storybook/src/stories/datacom-dropdown.stories.tsx
+++ apps/storybook/src/stories/datacom-dropdown.stories.tsx
@@ -25,14 +25,13 @@ export default {
       description: 'Disable Dropdown',
       type: { name: 'boolean' },
     },
-    required: {
-      name: 'Required',
-      description: 'Required Field',
+    isValid: {
+      name: 'Is Valid',
+      description: "Controls whether component is in it's error state.",
       type: { name: 'boolean' },
     },
     message: {
       name: 'Message',
-      defaultValue: 'Please select an option',
       description: 'Hover instruction text',
       type: { label: 'string' },
     },
@@ -40,37 +39,51 @@ export default {
 };
 
 const CountryTemplate: ComponentStoryFn<typeof DatacomDropdown> = (args) => (
-  <DatacomDropdown {...args}>
-    <DatacomOption
-      src="https://flagcdn.com/nz.svg"
-      value="NZ"
-      label="New Zealand"
-    />
-    <DatacomOption
-      src="https://flagcdn.com/au.svg"
-      value="AU"
-      label="Australia"
-    />
-    <DatacomOption
-      src="https://flagcdn.com/ki.svg"
-      value="KI"
-      label="Independent and Sovereign Republic of Kiribati"
-    />
-    <DatacomOption
-      src="https://flagcdn.com/gb.svg"
-      search="Great Britain | England | Wales | Scotland"
-      value="GB"
-      label="Great Britain"
-    />
-    <DatacomOption src="https://flagcdn.com/fr.svg" value="FR" label="France" />
-    <DatacomOption src="https://flagcdn.com/it.svg" value="IT" label="Italy" />
-    <DatacomOption src="https://flagcdn.com/es.svg" value="ES" label="Spain" />
-    <DatacomOption
-      src="https://flagcdn.com/ua.svg"
-      value="EA"
-      label="Ukraine"
-    />
-  </DatacomDropdown>
+  <div style={{ maxWidth: 272 }}>
+    <DatacomDropdown {...args}>
+      <DatacomOption
+        src="https://flagcdn.com/nz.svg"
+        value="NZ"
+        label="New Zealand"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/au.svg"
+        value="AU"
+        label="Australia"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/ki.svg"
+        value="KI"
+        label="Independent and Sovereign Republic of Kiribati"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/gb.svg"
+        search="Great Britain | England | Wales | Scotland"
+        value="GB"
+        label="Great Britain"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/fr.svg"
+        value="FR"
+        label="France"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/it.svg"
+        value="IT"
+        label="Italy"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/es.svg"
+        value="ES"
+        label="Spain"
+      />
+      <DatacomOption
+        src="https://flagcdn.com/ua.svg"
+        value="EA"
+        label="Ukraine"
+      />
+    </DatacomDropdown>
+  </div>
 );
 
 export const WithImages = CountryTemplate.bind({});
@@ -80,13 +93,19 @@ WithImages.args = {
 };
 
 const SimpleTemplate: ComponentStoryFn<typeof DatacomDropdown> = (args) => (
-  <DatacomDropdown {...args}>
-    <DatacomOption value="1" label="Option 1"></DatacomOption>
-    <DatacomOption value="2" label="Option 2"></DatacomOption>
-    <DatacomOption value="3" label="Option 3"></DatacomOption>
-    <DatacomOption value="4" label="Option 4"></DatacomOption>
-    <DatacomOption value="5" label="Option 5"></DatacomOption>
-  </DatacomDropdown>
+  <div style={{ maxWidth: 272 }}>
+    <DatacomDropdown {...args}>
+      <DatacomOption value="1" label="Option 1"></DatacomOption>
+      <DatacomOption value="2" label="Option 2"></DatacomOption>
+      <DatacomOption value="3" label="Option 3"></DatacomOption>
+      <DatacomOption value="4" label="Option 4"></DatacomOption>
+      <DatacomOption value="5" label="Option 5"></DatacomOption>
+    </DatacomDropdown>
+  </div>
 );
 
 export const Simple = SimpleTemplate.bind({});
+Simple.args = {
+  label: 'Options',
+  message: 'Please select an option',
+};
