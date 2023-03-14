a05f4c185141f2394185d4ebda52adaef1b1b25b
Sidra Imran <sidra.imran@datacom.co.nz>
2023-03-15 11:10:23 +1300

    Add the animation for the card and make the card clickable.

--- packages/stencil/src/components/datacom-display-card/datacom-display-card.tsx
+++ packages/stencil/src/components/datacom-display-card/datacom-display-card.tsx
@@ -19,20 +19,20 @@ export class DatacomDisplayCard {
     return (
       <Host>
         <div class="dc-display-card-wrapper" tabIndex={0}>
-          <div class="dc-display-card-media">
-            <div class="dc-display-card-image" style={{ backgroundImage: 'url(' + this.imageUrl + ')' }} />
-            <div class="dc-display-card-content">
-              <div class="dc-display-content">
-                <h5 class="dc-display-card-heading">{this.heading}</h5>
-                <span class="dc-display-card-text">
-                  <slot />
-                </span>
-                <a href={this.url} class="dc-display-card-link">
-                  {this.ctaText}
-                </a>
+          <a href={this.url} class="dc-display-card-link">
+            <div class="dc-display-card-media">
+              <div class="dc-display-card-image" style={{ backgroundImage: 'url(' + this.imageUrl + ')' }} />
+              <div class="dc-display-card-content">
+                <div class="dc-display-content">
+                  <h5 class="dc-display-card-heading">{this.heading}</h5>
+                  <span class="dc-display-card-text">
+                    <slot />
+                  </span>
+                  <span class="dc-display-card-cta-text">{this.ctaText}</span>
+                </div>
               </div>
             </div>
-          </div>
+          </a>
         </div>
       </Host>
     );
