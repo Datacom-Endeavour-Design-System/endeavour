90fb8eafa7b1c043c8176e946f98c07bdd5a25c8
Sidra Imran <sidra.imran@datacom.co.nz>
2023-03-20 10:29:47 +1300

    Bug fixes and add some style

--- packages/stencil/src/components/datacom-display-card/datacom-display-card.tsx
+++ packages/stencil/src/components/datacom-display-card/datacom-display-card.tsx
@@ -18,22 +18,24 @@ export class DatacomDisplayCard {
   render() {
     return (
       <Host>
-        <div class="dc-display-card-wrapper" tabIndex={0}>
-          <a href={this.url} class="dc-display-card-link">
-            <div class="dc-display-card-media">
-              <div class="dc-display-card-image" style={{ backgroundImage: 'url(' + this.imageUrl + ')' }} />
-              <div class="dc-display-card-content">
-                <div class="dc-display-content">
-                  <h5 class="dc-display-card-heading">{this.heading}</h5>
-                  <span class="dc-display-card-text">
-                    <slot />
-                  </span>
+        <a href={this.url} class="dc-display-card-link">
+          <div class="dc-display-card-media">
+            <div class="dc-display-card-image" style={{ backgroundImage: 'url(' + this.imageUrl + ')' }} />
+            <div class="dc-display-card-content">
+              <div class="dc-display-content">
+                <span class="dc-display-card-heading">{this.heading}</span>
+                <span class="dc-display-card-description">
+                  <slot />
+                </span>
+              </div>
+              <div>
+                <div class="dc-display-card-cta-wrapper">
                   <span class="dc-display-card-cta-text">{this.ctaText}</span>
                 </div>
               </div>
             </div>
-          </a>
-        </div>
+          </div>
+        </a>
       </Host>
     );
   }
