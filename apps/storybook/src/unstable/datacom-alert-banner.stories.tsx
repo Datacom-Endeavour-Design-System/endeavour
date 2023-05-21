a951b57e2111cc13b379e45be327ab5cf525a433
Sidra Imran <sidra.imran@datacom.co.nz>
2023-05-21 21:35:12 +1200

    feedback updates

--- apps/storybook/src/unstable/datacom-alert-banner.stories.tsx
+++ apps/storybook/src/unstable/datacom-alert-banner.stories.tsx
@@ -8,6 +8,22 @@ export default {
   title: 'Alert Banner',
   component: DatacomAlertBanner,
   argTypes: {
+    variant: {
+      name: 'Variant',
+      description: "Alert banner variant. Defaults to 'dark-blue' if not set.",
+      control: {
+        type: 'select',
+        labels: {
+          'dark-blue': 'Dark-blue',
+          blue: 'Datacom-blue',
+          'light-blue': 'Light-blue',
+          'primary-pink': 'Primary-accent-pink',
+        },
+      },
+      defaultValue: 'dark-blue',
+      options: ['blue', 'dark-blue', 'light-blue', 'primary-pink'],
+      type: { name: 'string' },
+    },
     message: {
       name: 'Message',
       defaultValue: 'A message to our Datacom community about COVID-19 -',
@@ -26,13 +42,12 @@ export default {
   },
 };
 
-const Template: StoryFn<AlertBannerProps> = (args) => {
-  const { message, label, url } = args;
+const Template: StoryFn<AlertBannerProps & { message: string }> = (args) => {
+  const { message } = args;
   return (
-    <DatacomAlertBanner
-      label={label}
-      url={url}
-      message={message}></DatacomAlertBanner>
+    <DatacomAlertBanner {...args}>
+      <span slot="message">{message}</span>
+    </DatacomAlertBanner>
   );
 };
 
--- packages/stencil/src/components/datacom-alert-banner/datacom-alert-banner.tsx
+++ packages/stencil/src/components/datacom-alert-banner/datacom-alert-banner.tsx
@@ -1,13 +1,15 @@
 import { Component, Host, h, Prop, State, Event, EventEmitter } from '@stencil/core';
 import { getSvg } from '../../common/images/icon-provider';
 
+export type AlertBannerVariant = 'dark-blue' | 'blue' | 'light-blue' | 'primary-pink';
+
 @Component({
   tag: 'datacom-alert-banner',
   styleUrl: 'datacom-alert-banner.css',
   shadow: true,
 })
 export class DatacomAlertBanner {
-  @Prop() message: string;
+  @Prop() variant: AlertBannerVariant = 'dark-blue';
   @Prop() label: string;
   @Prop() url: string;
   @State() isOpen = true;
@@ -22,12 +24,17 @@ export class DatacomAlertBanner {
   };
 
   render() {
+    const Classes = {
+      'dc-alert-banner': true,
+      [`dc-alert-banner-${this.variant}`]: true,
+    };
+
     return (
       <Host>
         {this.isOpen && (
-          <div class="dc-alert-banner">
+          <div class={Classes}>
             <div class="dc-alert-banner-content">
-              {this.message} &nbsp;
+              <slot name="message" /> &nbsp;
               <a href={this.url} class="dc-alert-banner-cta-text">
                 {this.label}
               </a>
