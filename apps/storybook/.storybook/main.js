95efe2e14c1a1f932fe87b7891d6ee9c8e3ac12e
Jon Vivian <jonv@datacom.co.nz>
2023-10-02 11:34:39 +1300

    update to latest stable storybook v7.4

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from 'path';
 const stories =
   process.env.STORYBOOK_STORIES === 'unstable'
     ? ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)']
@@ -5,14 +6,25 @@ const stories =
 
 module.exports = {
   stories,
+
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+
   staticDirs: ['../static'],
+
+  docs: {
+    autodocs: true,
+  },
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
