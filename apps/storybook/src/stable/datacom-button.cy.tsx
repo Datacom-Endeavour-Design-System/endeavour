57ed6e3c039484b18202d442119fbbc8cb2f58b9
Jon Vivian <jonv@datacom.co.nz>
2023-10-10 21:54:15 +1300

    fix tests

--- apps/storybook/src/stable/datacom-button.cy.tsx
+++ apps/storybook/src/stable/datacom-button.cy.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { composeStories } from '@storybook/react';
 import { mount } from 'cypress/react';
 import * as stories from './datacom-button.stories';
 
 // compile the "Primary" story with the library
-const { Button: Primary } = composeStories(stories);
+const { Primary } = composeStories(stories);
 
 it('Mounts', () => {
   // and mount the story using @cypress/react library
