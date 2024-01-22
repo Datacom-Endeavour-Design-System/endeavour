226bf2bf65cf40b16233dc7962ced45159eedd77
Sidra Imran <sidra.imran@datacom.co.nz>
2024-01-22 14:22:23 +1300

    Updated the description after feedback

--- apps/storybook/src/components/stable/datacom-display-card.stories.tsx
+++ apps/storybook/src/components/stable/datacom-display-card.stories.tsx
@@ -14,32 +14,25 @@ export default {
   argTypes: {
     heading: {
       name: 'Title',
-      defaultValue: 'Title',
       description: 'Text of title displayed.',
       type: { name: 'string', required: 'true' },
     },
     description: {
       name: 'Description',
-      defaultValue:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
       description: 'Description of display card.',
       type: { name: 'string' },
     },
     ctaText: {
       name: 'CTA Text',
-      defaultValue: 'Learn more',
       type: { name: 'string' },
     },
     url: {
       name: 'URL',
-      defaultValue: 'https://datacom.com',
       description: 'URL that  should link to.',
       type: { name: 'string' },
     },
     imageUrl: {
       name: 'Image URL',
-      defaultValue:
-        'https://images.pexels.com/photos/15638791/pexels-photo-15638791.jpeg',
       description: 'Image URL to be displayed at top of card.',
       type: { name: 'string', required: 'true' },
     },
@@ -47,7 +40,7 @@ export default {
   args: {
     heading: 'Title',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     ctaText: 'Learn more',
     url: 'https://datacom.com',
     imageUrl:
@@ -92,10 +85,7 @@ export const DisplayCardGroup = () => {
           url="https://datacom.com">
           <span slot="description">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
           </span>
         </DatacomDisplayCard>
@@ -105,9 +95,6 @@ export const DisplayCardGroup = () => {
           ctaText="Learn more"
           url="https://datacom.com">
           <span slot="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
