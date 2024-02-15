48c3c598eec25bdb4fa7b90e4d54ec63c02bbdf5
Louis Lingatong <louislingatong@gmail.com>
2024-02-15 16:25:38 +1300

    Update tabs component styling with the latest design

--- apps/storybook/src/components/stable/datacom-tab.stories.tsx
+++ apps/storybook/src/components/stable/datacom-tab.stories.tsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import { DatacomTabgroup, DatacomTab } from '@datacom/endeavour-react';
-import styled from '@emotion/styled';
 
 export default {
   title: 'Tabs',
   component: DatacomTabgroup,
 };
 
-/* TODO: var color */
-const H2 = styled.h2`
-  font-family: Montserrat;
-  font-size: 48px;
-  weight: 700;
-  color: #0a1839;
-`;
-
-/* TODO: var color */
-const P = styled.p`
-  font-family: Montserrat;
-  font-size: 16px;
-  weight: 400;
-  color: #0a1839;
-`;
-
 export const Tabs = () => {
   return (
     <>
       <DatacomTabgroup>
         <DatacomTab label="Overview">
-          <H2>Overview</H2>
-          <P>
+          <h2>Overview</h2>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -37,12 +20,12 @@ export const Tabs = () => {
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
-          </P>
+          </p>
         </DatacomTab>
 
         <DatacomTab label="Solutions">
-          <H2>Solutions</H2>
-          <P>
+          <h2>Solutions</h2>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             fringilla orci eget turpis scelerisque facilisis. Nullam feugiat non
             ex eu egestas. Pellentesque habitant morbi tristique senectus et
@@ -50,9 +33,9 @@ export const Tabs = () => {
             sapien elit. Vivamus dictum, mi quis malesuada consequat, diam lorem
             ullamcorper massa, a aliquam lacus est a eros. Phasellus varius nisi
             in felis viverra sollicitudin. In hac habitasse platea dictumst.
-          </P>
+          </p>
 
-          <P>
+          <p>
             Nullam placerat id diam sed ultrices. Integer blandit velit in
             vehicula posuere. Ut eleifend pulvinar tortor. Suspendisse congue
             eleifend dolor, et tempus nisl sagittis eu. Vestibulum ante ipsum
@@ -60,16 +43,16 @@ export const Tabs = () => {
             Cras egestas sagittis odio, vel accumsan dui varius id. Aliquam
             tempus sit amet risus eget pharetra. Mauris in ex finibus, iaculis
             eros et, pharetra urna.
-          </P>
+          </p>
         </DatacomTab>
 
         <DatacomTab label="Industries" disabled={true}>
-          <H2>Industries</H2>
+          <h2>Industries</h2>
         </DatacomTab>
 
         <DatacomTab label="Discover">
-          <H2>Discover</H2>
-          <P>
+          <h2>Discover</h2>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at
             pharetra nisi, at laoreet nisi. Pellentesque porta rutrum dolor, vel
             iaculis erat pretium nec. Vestibulum mattis magna sagittis tellus
@@ -81,7 +64,7 @@ export const Tabs = () => {
             eleifend. Sed tincidunt porttitor elit nec ultrices. In suscipit
             lorem purus, non mollis lacus tincidunt vel. Mauris nec condimentum
             nisl.
-          </P>
+          </p>
         </DatacomTab>
       </DatacomTabgroup>
     </>
